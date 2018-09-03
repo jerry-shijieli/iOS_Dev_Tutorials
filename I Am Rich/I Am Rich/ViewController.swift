b8bed09c31755348dad5e2090d65a63928514813
sli41 <sli41@ncsu.edu>
2018-09-02 17:25:03 -0700

    Build I Am Rick App And Run test on iPad

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jerry on 9/2/18.
+//  Copyright © 2018 AppTweaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
