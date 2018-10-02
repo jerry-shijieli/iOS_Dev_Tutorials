59349fac7a79b452c924c52c271d88dd64cfd32e
sli41 <sli41@ncsu.edu>
2018-10-02 00:13:38 -0700

    Create a item data model
    Use this item data model to solve reusable cell checkmark problem.
    However, it will fail the UserDefaults persistent storage due to the
    customize data type.

--- Todoey/Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Todoey/Controllers/TodoListViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
-    let storageKey : String = "TodoListArray" // key for local storage retrieval
-    let defaults = UserDefaults.standard // local storage
+    var itemArray = [Item]()
+//    let storageKey : String = "TodoListArray" // key for local storage retrieval
+//    let defaults = UserDefaults.standard // local storage
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: storageKey) as? [String]{
-            itemArray = items
-        }
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem1 = Item()
+        newItem1.title = "Buy Eggs"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Destroy Demogorgon"
+        itemArray.append(newItem2)
+        
+//        if let itemsData = defaults.object(forKey: storageKey) as? NSData {
+//            if let items = (NSKeyedUnarchiver.unarchiveObject(with: itemsData as Data) as? [Item]){
+//                itemArray = items
+//            }
+//        }
     }
 
     // MARK: - Table view data source
@@ -33,19 +47,19 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
 
-         cell.textLabel?.text = itemArray[indexPath.row]
-
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
     // MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -59,8 +73,12 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default, handler: {
             (action) in
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: self.storageKey)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+//            guard let itemsData = try? NSKeyedArchiver.archivedData(withRootObject: self.itemArray, requiringSecureCoding: false)
+//            self.defaults.set(itemsData, forKey: self.storageKey)
             self.tableView.reloadData()
         })
         
---
+++ Todoey/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jerry on 10/1/18.
+//  Copyright © 2018 AppTweaker. All rights reserved.
+//
+
+import Foundation
+
+class Item: NSObject {
+    var title : String = ""
+    var done : Bool = false
+}
