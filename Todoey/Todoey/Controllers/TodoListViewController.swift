6c51f13642254e05947f391d3518ae40b09c181a
sli41 <sli41@ncsu.edu>
2018-10-07 23:51:44 -0700

    Implemented UISearchBar and query Core Data
    Use extension to extend TodoListViewController with
    UISearchBarDelegate; Created fetch request and set predicate based on
    search bar text content; set sortDescriptors to display sorted request
    results.

--- Todoey/Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Todoey/Controllers/TodoListViewController.swift
@@ -103,12 +103,28 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context \(error)")
         }
+        
+        tableView.reloadData()
+    }
+}
+
+
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
     }
 }
