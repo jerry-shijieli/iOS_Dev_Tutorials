eb9ad204f6fbf96e3018eb956474f15abe5f84ba
sli41 <sli41@ncsu.edu>
2018-10-23 00:11:32 -0700

    Implemented basic sign key using setter and getter
    Digit key has bug for ‘.’ empty value, should give a default 0.

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,12 +12,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    var isFinishedTypingNumber: Bool = true
+    private var isFinishedTypingNumber: Bool = true
+    
+    // using getter and setter to refactor code and change value automatically
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert to Double!")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            switch (calcMethod) {
+            case "+/-": displayValue *= -1
+            case "AC": displayLabel.text = "0"
+            case "%": displayValue *= 0.01
+            default: displayValue *= 1
+            }
+        }
     }
 
     
@@ -29,6 +51,10 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {return}
+                }
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
