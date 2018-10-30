1164d44ede7b001d515053d3084eeb163acbde57
sli41 <sli41@ncsu.edu>
2018-10-29 23:50:10 -0700

    Import image from Camera or Photo Library
    Set up UI components: ImageView and Bar Button Item to get photo and
    show in view; inherit delegate for image picker and configure
    imagePicker settings; Configure Info.plist privacy properties and use
    UIImagePickerController to obtain image.

---
+++ SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Jerry on 10/27/18.
+//  Copyright © 2018 AppTweaker. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
