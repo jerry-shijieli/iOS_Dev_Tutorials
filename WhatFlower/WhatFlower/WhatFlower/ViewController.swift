670adbd30cf158af2c4f7d054b45bbf8fc382b10
sli41 <sli41@ncsu.edu>
2018-11-02 20:49:29 -0700

    Get Classification from model

--- WhatFlower/WhatFlower/WhatFlower/ViewController.swift
+++ WhatFlower/WhatFlower/WhatFlower/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -23,11 +25,42 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
-        
-        imageView.image = userPickedImage as? UIImage
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            
+            imageView.image = userPickedImage
+            
+            detect(flowerImage: ciImage)
+        }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(flowerImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Could load ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) {
+            (request, error) in
+            
+            guard let result = request.results?.first as? VNClassificationObservation else {
+                fatalError("Unable to perform classification")
+            }
+            
+            self.navigationItem.title = result.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
