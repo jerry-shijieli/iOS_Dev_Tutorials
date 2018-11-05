b73f965e75d6049bdc4e55d1116f18b1316018d9
sli41 <sli41@ncsu.edu>
2018-11-05 00:29:18 -0800

    Create a 3D cube
    Using SCNBox, SCNMaterial, SCNNode, SCNVector3 and sceneView in
    SceneKit.

--- AR_Dicee/AR_Dicee/ViewController.swift
+++ AR_Dicee/AR_Dicee/ViewController.swift
@@ -20,11 +20,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
