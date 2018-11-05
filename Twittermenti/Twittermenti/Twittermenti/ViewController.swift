d8783f3595870c476ba6700e5bb93810f15b342b
sli41 <sli41@ncsu.edu>
2018-11-04 21:42:04 -0800

    Parse Tweet JSON and Make predictions using NLP model and update UI label

--- Twittermenti/Twittermenti/Twittermenti/ViewController.swift
+++ Twittermenti/Twittermenti/Twittermenti/ViewController.swift
@@ -22,36 +22,79 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let filepath = Bundle.main.path(forResource: "secret", ofType: "plist") else {
-            fatalError("Cannot find the Property List file for API Keys!")
-        }
-        
-        if let keys = NSDictionary(contentsOfFile: filepath) {
-            if let API_Key = keys["API_Key"] as? String, let API_Secret_Key = keys["API_Secret_Key"] as? String {
-            
-                swifter = Swifter(consumerKey: API_Key, consumerSecret: API_Secret_Key)
-                
-                let prediction = try! sentimentClassifier.prediction(text: "@apple is good")
-                print(prediction.label)
-                
-                swifter?.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-                    //print(results)
-                }) { (error) in
-                    print("There is an error with API request, \(error)")
-                }
-                
-            } else {
-                fatalError("Cannot find API Keys!")
-            }
-        } else {
-            fatalError("Cannot find API Keys!")
-        }
+
     }
 
     @IBAction func predictPressed(_ sender: Any) {
     
-    
+        if let searchText = textField.text {
+            guard let filepath = Bundle.main.path(forResource: "secret", ofType: "plist") else {
+                fatalError("Cannot find the Property List file for API Keys!")
+            }
+            
+            if let keys = NSDictionary(contentsOfFile: filepath) {
+                if let API_Key = keys["API_Key"] as? String, let API_Secret_Key = keys["API_Secret_Key"] as? String {
+                    
+                    swifter = Swifter(consumerKey: API_Key, consumerSecret: API_Secret_Key)
+                    
+                    swifter?.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+                        
+                        var tweets = [TwitterSentimentClassifierInput]()
+                        var sentimentScore : Double = 0.0
+
+                        if let resultTweets = results.array {
+                            for i in 0..<resultTweets.count {
+                                if let tweetText = resultTweets[i]["full_text"].string {
+                                    print(tweetText)
+                                    let tweetForClassification = TwitterSentimentClassifierInput(text: tweetText)
+                                    tweets.append(tweetForClassification)
+                                }
+                            }
+                        }
+
+                        do {
+                            let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                                for pred in predictions {
+                                    switch(pred.label) {
+                                    case "Pos": sentimentScore += 1
+                                    case "Neg": sentimentScore -= 1
+                                    default: sentimentScore += 0
+                                    }
+                                }
+                        }catch {
+                                print("Cannot make prediction!")
+                        }
+
+                        sentimentScore = sentimentScore / Double((tweets.count+1)) * 100
+                        print(sentimentScore)
+                        
+                        if sentimentScore > 20 {
+                            self.sentimentLabel.text = "😍"
+                        } else if sentimentScore > 10 {
+                            self.sentimentLabel.text = "😀"
+                        } else if sentimentScore > 0 {
+                            self.sentimentLabel.text = "🙂"
+                        } else if sentimentScore == 0 {
+                            self.sentimentLabel.text = "😑"
+                        } else if sentimentScore > -10 {
+                            self.sentimentLabel.text = "☹️"
+                        } else if sentimentScore > -20 {
+                            self.sentimentLabel.text = "😡"
+                        } else {
+                            self.sentimentLabel.text = "👿"
+                        }
+                        
+                    }) { (error) in
+                        print("There is an error with API request, \(error)")
+                    }
+                    
+                } else {
+                    fatalError("Cannot find API Keys!")
+                }
+            } else {
+                fatalError("Cannot find API Keys!")
+            }
+        }
     }
     
 }
