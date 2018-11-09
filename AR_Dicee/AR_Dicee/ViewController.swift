6b745de24fd4a5500243cdc115e12de32d265ba2
sli41 <sli41@ncsu.edu>
2018-11-09 01:06:24 -0800

    Roll all dices and clear all dices
    Keep all dice SCNNodes in array; Use Bar Button Item as well as motion
    to roll all dices and another Bar Button Item to remove all dices to
    reset AR scene.

--- AR_Dicee/AR_Dicee/ViewController.swift
+++ AR_Dicee/AR_Dicee/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var diceArray = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -55,6 +57,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
                 
                 if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    
                     diceNode.position = SCNVector3(
                         x: hitResult.worldTransform.columns.3.x,
                         y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
@@ -62,17 +65,47 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                     
                     sceneView.scene.rootNode.addChildNode(diceNode)
                     
-                    let randomX = Float(arc4random_uniform(4)+1) * (Float.pi/2)
-                    
-                    let randomZ = Float(arc4random_uniform(4)+1) * (Float.pi/2)
-                    
-                    diceNode.runAction(SCNAction.rotateBy(x: CGFloat(randomX*5), y: 0, z: CGFloat(randomZ*5), duration: 0.5))
+                    diceArray.append(diceNode)
                     
                 }
             }
         }
     }
+    
+    func rollAll () {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode) {
+        
+        let randomX = Float(arc4random_uniform(4)+1) * (Float.pi/2)
+        
+        let randomZ = Float(arc4random_uniform(4)+1) * (Float.pi/2)
+        
+        dice.runAction(SCNAction.rotateBy(x: CGFloat(randomX*5), y: 0, z: CGFloat(randomZ*5), duration: 0.5))
+    }
 
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
+    @IBAction func resetScene(_ sender: UIBarButtonItem) {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                dice.removeFromParentNode()
+            }
+            diceArray.removeAll()
+        }
+    }
+    
     // MARK: - Scene View Delegate function
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
